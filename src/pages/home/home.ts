6ab4e438fca3861a659b52f7c518a72b274277af
MARK FLAHERTY <G00356549@gmit.ie>
2019-03-13 17:39:02 +0000

    lab upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {MovieProvider} from '../../providers/movie/movie';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+movies:any = [];
+  constructor(public navCtrl: NavController,
+  private movieProvider:MovieProvider) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad(){
+    this.movieProvider.getFilmInfo().subscribe((data)=>{
+      this.movies = data.Search;
+
+    });
   }
 
 }
